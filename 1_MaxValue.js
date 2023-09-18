6542b05e492f2103c812f28475c990af865bd9b7
YooJin Kim <lily@YooJins-Laptop.local>
2023-09-18 22:40:03 +0900

    pair 02

---
+++ 1_MaxValue.js
@@ -0,0 +1,14 @@
+
+let numbers_array = [1, 2, 3, 4, 5];
+
+const maxValue = Math.max(numbers_array);
+console.log("Max value is: " + maxValue);
+
+console.log("Original Array: ");
+console.log(numbers_array)
+
+numbers_array.reverse();
+
+console.log("Reversed Array: ");
+console.log(numbers_array); 
+
